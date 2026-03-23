6c3e1d6f87fbb88b5badaa2d3c43dea9e49fed98
Estkir <estebankirmser@gmail.com>
2026-03-23 15:05:55 +0100

    Update vyos_config.sh

--- vyos/vyos_config.sh
+++ vyos/vyos_config.sh
@@ -1,1 +1,7 @@
-
+configure
+set interfaces ethernet eth0 vif 10 address 192.168.10.1/24
+set interfaces ethernet eth0 vif 20 address 192.168.20.1/24
+set interfaces ethernet eth0 vif 30 address 192.168.30.1/24
+commit
+save
+exit
